2219077e3bd617a2665adc926277254a4f56fbee
Jason Chionh <jasonchionh@gmail.com>
2012-05-01 00:52:10 +0800

    - initial commit of the simple webworker test

---
+++ main.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// when the workers post back the results via the postMessage API
+// they will call this function to update the DOM with the results
+// computed
+function workerResultHandler(e) {
+    // read the results to local
+    // do not really need to do this, but just for readibility
+    // and debugging
+    var id = e.data.id;
+    var result = e.data.result;
+    var var1 = e.data.var1;
+    var var2 = e.data.var2;
+    
+    // jQuery fnd the div and set the text
+    var resultDiv = $('<div/>');
+    resultDiv.text('[' + id + '] recv result: ' + var1 + ' + ' + var2 + ' = ' + result + ' timestamp: ' + new Date().toUTCString());
+    $('#output_area').append(resultDiv);
+}
+
+
+
+// this is the main entry to the javascript
+function mainInit() {
+    
+    // let's spawn 6 workers!
+    var workers = [];
+    for (var i = 0; i < 6; ++i) {
+        var worker = new Worker('worker1.js');        
+        worker.addEventListener('message', workerResultHandler ,false);
+        workers.push(worker);
+    }
+    
+    // now give them a task to do, perform, an addition
+    for (var j = 0; j < 6; ++j) {
+        
+        // randomize the input vars
+        var var1 = Math.floor(Math.random() * 100);
+        var var2 = Math.floor(Math.random() * 100);
+        
+        // record the input on the DOM
+        $('#input_' + j).text('[' + j + '] sending work: ' + var1 + ' + ' + var2 + ' timestamp: ' + new Date().toUTCString());
+        
+        // post the vars to the respective worker for them to perform the computation
+        workers[j].postMessage({id: j, var1: var1, var2: var2});
+    }
+}
---
+++ worker1.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// everything in this file will be excuted as a seperate thread
+
+
+// use the event listener API to recieve a on message event
+self.addEventListener('message', function(e) {
+  
+  // read off the input values posted to the worker
+  var id = e.data.id;
+  
+  // parse int just in case
+  var var1 = parseInt(e.data.var1, 10);
+  var var2 = parseInt(e.data.var2, 10);
+  
+  // perform our computation to get the result
+  var result = var1 + var2;
+  
+  // once we get the result, post the results back to the main thread
+  self.postMessage({id: id, var1: var1, var2: var2, result: result});
+  
+}, false);
